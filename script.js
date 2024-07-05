ace2ae26efb0a1648707047d4f64bfb2cf81fe7c
Vansh <vanshkathnawal0942@gmail.com>
2024-07-05 21:28:07 +0530

    some modificatinos made

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let player_scoredom = document.querySelector('.player_score');
 let computer_scoredom = document.querySelector('.computer_score');
 let round_result = document.querySelector('.result');
 let game_status = document.querySelector('#status');
+let pc_img = document.querySelector('#player-choice-img');
+let cc_img = document.querySelector('#computer-choice-img');
+
+
 
 choices.forEach((choice) => {
     choice.addEventListener("click",() =>{
@@ -15,29 +19,52 @@ choices.forEach((choice) => {
         if(str=='Paper') xp=1;
         if(str=='Scissor') xp=2;
         player_choicedom.textContent = `Player Choice : ${choice.textContent}`;
-        if(xc==0){
-            computer_choicedom.textContent = "Computer's Choice : Rock";
-        }else if(xc==1){
-            computer_choicedom.textContent = "Computer's Choice ; Paper";
-        }else if(xc==2){
-            computer_choicedom.textContent = "Computer's Choice ; Scissor";
-        }
-        let res = PlayRound(xp,xc);
-        if(res==-1){
-            round_result.textContent = "Computer Won this round";
-        }else if(res==1)
-        {
-            round_result.textContent = "Player won this round";
-        }else{
-            round_result.textContent = "It's a Draw";
-        }
-        if(playerscore>=3){
-           window.location.href='player-won.html';
-        }else if (computerscore>=3){
-            window.location.href='player-lost.html';
-        }
-    })
-})
+        let path = choice.textContent + '.png';
+        pc_img.src=path;
+        pc_img.classList.remove('invisible');
+        cc_img.src='';
+        cc_img.classList.add('invisible');
+        computer_choicedom.textContent = "Computer's Choice : Loading ....";
+        round_result.classList.remove(...round_result.classList);
+        round_result.textContent = "Result";
+        
+        setTimeout(()=> {
+            cc_img.classList.remove('invisible');
+            if(xc==0){
+                computer_choicedom.textContent = "Computer's Choice : Rock";
+                cc_img.src = "Rock.png";
+            }else if(xc==1){
+                computer_choicedom.textContent = "Computer's Choice ; Paper";
+                cc_img.src = "Paper.png";
+
+            }else if(xc==2){
+                computer_choicedom.textContent = "Computer's Choice ; Scissor";
+                cc_img.src = "Scissor.png";
+            }
+            let res = PlayRound(xp,xc);
+            if(res==-1){
+                round_result.textContent = "Computer Won this round";
+                round_result.classList.add("lost");
+            }else if(res==1)
+            {
+                round_result.textContent = "Player won this round";
+                round_result.classList.add("won");
+            }else{
+                round_result.textContent = "It's a Draw";
+            }
+            if(playerscore>=3){
+                setTimeout(() => {
+                     window.location.href='player-won.html';
+                },1000)
+            }else if (computerscore>=3){
+                setTimeout(() => {
+                    window.location.href='player-lost.html';
+               },1000)
+            }
+        },2000);
+ 
+    });
+});
 
 
 function getComputerChoice(){
